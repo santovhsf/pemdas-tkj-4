fe7cff94097286e3f8625dabb94cfef302307233
santovhsf <115443461+santovhsf@users.noreply.github.com>
2022-10-09 21:29:17 -0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,25 @@
+# membuat Tipe data koleksi
+
+koleksi_data_str=["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int=[200,300,400,500]
+
+koleksi_data_mix=["Rizky billar", 100, True,"Reza arap"]
+
+print("koleksi data string:",koleksi_data_str)
+
+print("koleksi data intger:",koleksi_data_int)
+
+print("koleksi data campuran:",koleksi_data_mix)
+
+koleksi_nama_hewan =["gajah","harimau","singa"]
+
+koleksi_nilai_uts =[77,89,90]
+
+koleksi_teman_sebangku =["ridho","isal","pai"]
+
+print("Koleksi nama Hewan:", koleksi_nama_hewan)
+
+print("Koleksi nilai UTS:", koleksi_nilai_uts)
+
+print("Koleksi teman Sebangku:", koleksi_teman_sebangku)
